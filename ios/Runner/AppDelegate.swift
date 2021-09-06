6dc3db1bd5b4b9ce7f6fe75701b1da9d86834796
giuslape <giuseppe@balzo.eu>
2021-09-06 10:46:37 +0200

    Test no physical device

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,34 +4,37 @@ import WatchConnectivity
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate, WCSessionDelegate {
+    
+    private var channel: FlutterMethodChannel!
+    
     func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
-        
+        print("State of session: \(activationState.rawValue)")
     }
     
     func sessionDidBecomeInactive(_ session: WCSession) {
-        
+        print("Inactive session")
     }
     
     func sessionDidDeactivate(_ session: WCSession) {
-        
+        print("Deactivate session")
     }
     
-    
-
     func sendString(text: String) {
         print(text)
         let session = WCSession.default
-        
+    
         if(session.isPaired && session.isReachable) {
             DispatchQueue.main.async {
                 print("Sending data to watch")
-                session.sendMessage(["data": text], replyHandler: nil, errorHandler: nil)
+                session.sendMessage(["data": text], replyHandler: nil, errorHandler: { error in
+                    print("\(error)")
+                    self.channel.invokeMethod("WatchKitError", arguments: error.localizedDescription)
+                })
             }
         } else {
             print("Watch not reachable")
         }
     }
-    
 
     override func application(
         _ application: UIApplication,
@@ -40,7 +43,7 @@ import WatchConnectivity
     
         let controller: FlutterViewController = window?.rootViewController as! FlutterViewController
         
-        let channel = FlutterMethodChannel(name: "it.gioelepannetto.triathlon_app/watch_channel", binaryMessenger: controller as! FlutterBinaryMessenger)
+        channel = FlutterMethodChannel(name: "it.gioelepannetto.triathlon_app/watch_channel", binaryMessenger: controller as! FlutterBinaryMessenger)
         
         channel.setMethodCallHandler({
             (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
@@ -51,16 +54,12 @@ import WatchConnectivity
             }
         })
         
-        
         if(WCSession.isSupported()) {
             let session = WCSession.default
             session.delegate = self
             session.activate()
         }
-    
-    
-    
-    
+        
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/native/WatchChannel.dart
+++ lib/native/WatchChannel.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/services.dart';
 
 class WatchChannel {
+  
+  final void Function(String error) callback;
+
+  
+  
   static const _channel = const MethodChannel('it.gioelepannetto.triathlon_app/watch_channel');
+
+  WatchChannel(this.callback) {
+    _channel.setMethodCallHandler((call) {
+      switch (call.method) {
+        case 'WatchKitError':
+          this.callback(call.arguments as String);
+          break;
+        default:
+          throw Exception();
+      }
+
+      return Future.value(null);
+    });
+  }
 
   static Future<void> sendString(String data) async {
     String res = await _channel.invokeMethod('sendString', data);
     print(res);
   }
+
 }
